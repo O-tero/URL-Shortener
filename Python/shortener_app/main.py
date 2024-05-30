77f9c48fb4543972b9872eba171fabad75e74baa
Otero <oteronickbrian@gmail.com>
2024-05-30 14:41:06 +0300

    connecting to database

--- Python/shortener_app/main.py
+++ Python/shortener_app/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+import validators
+from fastapi import FastAPI, HTTPException
+
+from . import schemas
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return "Welcome to the URL shortener NBA :)"
+
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+    return f"TODO: Create database entry for: {url.target_url}"
