bccd8cd35407af518427cc641a518aee4c0fec61
Otero <oteronickbrian@gmail.com>
2024-05-30 23:16:42 +0300

    refactor your code

--- Python/shortener_app/main.py
+++ Python/shortener_app/main.py
@@ -1,9 +1,10 @@
 import secrets
 import validators
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
-from . import schemas
+from . import models, schemas
 from .database import SessionLocal, engine
 
 app = FastAPI()
@@ -18,13 +19,28 @@ def get_db():
         db.close()
 
 
-@app.get("/")
-def read_root():
-    return "Welcome to the URL shortener NBA :)"
-
-
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
+
+
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist"
+    raise HTTPException(status_code=404, detail=message)
+
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+    url_key: str, request: Request, db: Session = Depends(get_db)
+):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
 
 
 @app.post("/url", response_model=schemas.URLInfo)
